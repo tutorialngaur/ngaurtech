5784864bfa51488f854bfa935383cbeb821a42e0
tutorialngaur <96699863+tutorialngaur@users.noreply.github.com>
2022-01-10 21:52:27 +0700

    Update and rename README.md to tech.sh

---
+++ tech.sh
@@ -0,0 +1,45 @@
+sudo adduser mbahngaur --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "mbahngaur:123" | sudo chpasswd
+sudo usermod -aG sudo,adm mbahngaur
+clear
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/32BvV4p > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389  > /dev/null 2>&1
+echo "===================================="
+echo "Install XFCE"
+echo "===================================="
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "=============50%"
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal
+echo "=================70%"
+echo xfce4-session >~/.xsession
+echo "=====================90%"
+sudo service xrdp start
+clear
+echo "=======================100%"
+echo "===================================="
+echo "Username : mbahngaur"
+echo "Password : 123"
+echo "right click to open menu"
+echo "SALAM NGAUR.COM DON'T CLOSE THIS TAB"
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+sleep 9876543210
