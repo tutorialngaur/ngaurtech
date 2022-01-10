4fa0694a56b1d0b893f27b9beb76eceb200a4dec
tutorialngaur <96699863+tutorialngaur@users.noreply.github.com>
2022-01-10 22:15:16 +0700

    Update tech.sh

--- tech.sh
+++ tech.sh
@@ -2,24 +2,6 @@ sudo adduser mbahngaur --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --dis
 echo "mbahngaur:123" | sudo chpasswd
 sudo usermod -aG sudo,adm mbahngaur
 clear
-rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/32BvV4p > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3389  > /dev/null 2>&1
-echo "===================================="
 echo "Install XFCE"
 echo "===================================="
 sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
@@ -41,5 +23,23 @@ echo "right click to open menu"
 echo "SALAM NGAUR.COM DON'T CLOSE THIS TAB"
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/32BvV4p > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp 3389
 echo "===================================="
 sleep 9876543210
